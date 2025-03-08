86b46e5d6de7cf06533a9fe46a946ae47b57476c
bernardinoleite <bernardinoleite2008@gmail.com>
2025-03-08 23:13:21 +0100

    chore: create AppError class for standardized error handling

---
+++ src/shared/error/appError.ts
@@ -0,0 +1,14 @@
+
+
+
+export class AppError extends Error {
+
+    public readonly message: string;
+    public readonly statusCode: number;
+
+    constructor(message: string, statusCode: number = 400) {
+        super(message)
+        this.message = message
+        this.statusCode = statusCode
+    }
+}
